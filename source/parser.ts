9b2b030ead02986f23263bb956dff48e9d97839b
TechQuery <shiy007@qq.com>
2020-05-16 22:29:31 +0800

    [add] Arguments parser & Project framework

---
+++ source/parser.ts
@@ -0,0 +1,43 @@
+export type PrimitiveData = string | number | boolean | null;
+
+export type Data =
+    | PrimitiveData
+    | Record<string, PrimitiveData>
+    | PrimitiveData[];
+
+export function parseData(raw: string): Data {
+    try {
+        return JSON.parse((raw = raw.trim()));
+    } catch {
+        return raw.includes(',')
+            ? (raw.split(',').map(parseData) as PrimitiveData[])
+            : raw;
+    }
+}
+
+export type OptionData<T> = Record<keyof T, Data>;
+
+export function parseArguments<T>(list: string[]) {
+    const options: OptionData<T> = {} as OptionData<T>,
+        data: Data[] = [];
+    let lastKey = '';
+
+    for (const item of list)
+        if (item.startsWith('--')) {
+            lastKey = item.slice(2);
+            options[lastKey as keyof T] = true;
+        } else if (item.startsWith('-')) {
+            if (item[2])
+                for (const k of item.slice(1)) options[k as keyof T] = true;
+            else {
+                lastKey = item[1];
+                options[lastKey as keyof T] = true;
+            }
+        } else if (lastKey) {
+            options[lastKey as keyof T] = parseData(item);
+            lastKey = '';
+        } else {
+            data.push(parseData(item));
+        }
+    return { options, data };
+}
---
+++ test/parser.spec.ts
@@ -0,0 +1,34 @@
+import { parseData, parseArguments } from '../source/parser';
+
+describe('Data parser', () => {
+    it('should parse String to Data', () => {
+        expect(parseData('1')).toBe(1);
+        expect(parseData('2x')).toBe('2x');
+        expect(parseData('1,2x')).toEqual(expect.arrayContaining([1, '2x']));
+    });
+
+    it('should parse Command-line arguments to an Object', () => {
+        expect(
+            parseArguments([
+                '-a',
+                '-b',
+                '0',
+                '-cd',
+                '--test',
+                '--example',
+                '0,1',
+                'sample'
+            ])
+        ).toMatchObject({
+            options: {
+                a: true,
+                b: 0,
+                c: true,
+                d: true,
+                test: true,
+                example: [0, 1]
+            },
+            data: ['sample']
+        });
+    });
+});
